6415452a529a2e5420152687e833f5f4f518befd
Marcos Paulo <marcos.poo@gmail.com>
2024-09-08 23:36:27 -0300

    Aula 2.2 - Conversor em estrelas

---
+++ screen-match/src/Calculos/ConversorNotaEstrela.php
@@ -0,0 +1,13 @@
+<?php
+
+class ConversorNotaEstrela 
+{
+    public function converte(Avaliavel $avaliavel): float
+    {
+        $nota = $avaliavel->media();
+
+        // Realiza a conversão
+
+        return round($nota) / 2;
+    }
+}
