543c484139bcfe820069083b4aa76a2a52638710
Marcos Paulo <marcos.poo@gmail.com>
2024-09-14 23:19:34 -0300

    Aula 4.2 - Tratamento de exceções

--- screen-match/erro.php
+++ screen-match/erro.php
@@ -9,6 +9,7 @@ require 'autoload.php';
 
 $serie = new Serie('Nome da serie',2024, Genero::Acao, 7, 20, 30);
 $episodio = new Episodio($serie, 'Piloto', 1);
+//$episodio->avalia(10);
 
 $conversor = new ConversorNotaEstrela();
 echo $conversor->converte($episodio);
--- screen-match/src/Calculos/ConversorNotaEstrela.php
+++ screen-match/src/Calculos/ConversorNotaEstrela.php
@@ -2,14 +2,21 @@
 
 namespace ScreenMatch\Calculos;
 
+use DivisionByZeroError;
 use ScreenMatch\Model\Avaliavel;
 
 class ConversorNotaEstrela 
 {
     public function converte(Avaliavel $avaliavel): float
     {
-        $nota = $avaliavel->media();
+        try {
+            $nota = $avaliavel->media();
 
-        return round($nota) / 2;
+            return round($nota) / 2;
+        } 
+        catch (DivisionByZeroError $erro) {
+            echo $erro->getMessage();
+            return 0;
+        }
     }
 }
